d488b59050d87f595bbea638fbe1899bd685bede
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-07-11 11:33:49 +0800

    feat(system): add network ports configuration

---
+++ modules/core/system/ports.nix
@@ -0,0 +1,17 @@
+{ lib, config, inputs, ... }:
+let
+  portsCfg = import "${inputs.my-secrets}/network/ports.nix" { inherit lib; };
+in
+{
+  options.port' = lib.mkOption {
+    type = with lib.types; attrsOf port;
+    default = portsCfg.port;
+    description = "Network port configurations";
+  };
+
+  options.portStr' = lib.mkOption {
+    type = with lib.types; attrsOf str;
+    default = portsCfg.portStr;
+    description = "Network port configurations as strings";
+  };
+}
