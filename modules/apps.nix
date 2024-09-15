6b8e4e3c9c71d0a310d12f25c6aa6b76ca517d39
Aaron <85681241+27Aaron@users.noreply.github.com>
2024-09-15 18:45:36 +0800

    feat: add brew

--- modules/apps.nix
+++ modules/apps.nix
@@ -7,4 +7,60 @@
     nixfmt-rfc-style
   ];
   environment.variables.EDITOR = "nvim";
+
+  # The apps installed by homebrew are not managed by nix, and not reproducible!
+  # But on macOS, homebrew has a much larger selection of apps than nixpkgs, especially for GUI apps!
+  homebrew = {
+    enable = true;
+
+    onActivation = {
+      autoUpdate = false;
+      # 'zap': uninstalls all formulae(and related files) not listed here.
+      cleanup = "zap";
+    };
+
+    # Applications to install from Mac App Store using mas.
+    # You need to install all these Apps manually first so that your apple account have records for them.
+    # otherwise Apple Store will refuse to install them.
+    # For details, see https://github.com/mas-cli/mas 
+    masApps = {
+      Bob = 1630034110;
+      # Xcode = 497799835;
+      Enpass = 732710998;
+    };
+
+    taps = [
+      "owo-network/brew"
+    ];
+
+    # `brew install`
+    brews = [
+      "wget"
+      "curl"
+      "deeplx"
+      "lazygit"
+      "node"
+    ];
+
+    # `brew install --cask`
+    casks = [
+      "battery"
+      "macs-fan-control"
+      "raycast"
+      "visual-studio-code"
+      "termius"
+      "google-chrome"
+      "karabiner-elements"
+      "microsoft-remote-desktop"
+      "telegram"
+      "wechat"
+      "wechatwork"
+      "qspace-pro"
+      "surge"
+      "font-lxgw-wenkai"
+      "monitorcontrol"
+      "zulu@8"
+      "zulu@17"
+    ];
+  };
 }
