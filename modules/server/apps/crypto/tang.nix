f8fd777d051a90a8a5b58b6d3054a579dc9a20dc
Aaron <85681241+27Aaron@users.noreply.github.com>
2025-03-26 11:33:48 +0800

    feat(server): add Tang key derivation service

---
+++ modules/server/apps/crypto/tang.nix
@@ -0,0 +1,25 @@
+{ lib, config, ... }:
+let
+  cfg = config.services'.tang;
+in
+{
+  options.services'.tang = {
+    enable = lib.mkEnableOption "Enable Tang key derivation service";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.tang = {
+      enable = true;
+      listenStream = [ config.portStr'.tang ];
+      ipAddressAllow = [
+        "0.0.0.0/0"
+      ];
+    };
+
+    preservation'.os.directories = [
+      "/var/lib/private/tang"
+    ];
+
+    networking.firewall.allowedTCPPorts = [ config.port'.tang ];
+  };
+}
