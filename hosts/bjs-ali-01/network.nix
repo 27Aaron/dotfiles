e96c356c22a4c00527f78f8b6aaf6f9d94c4a3ef
Aaron <85681241+27Aaron@users.noreply.github.com>
2025-10-18 15:33:26 +0800

    feat(hosts): add bjs-ali-01 network configuration

---
+++ hosts/bjs-ali-01/network.nix
@@ -0,0 +1,24 @@
+{ lib, ... }:
+{
+  systemd.network.enable = true;
+  networking.useDHCP = false;
+  networking.useNetworkd = true;
+  networking.resolvconf.enable = false;
+
+  services.resolved.settings.Resolve = {
+    LLMNR = false;
+    MulticastDNS = false;
+    Cache = "no-negative";
+  };
+
+  environment.etc."resolv.conf".text = lib.mkForce ''
+    nameserver 223.5.5.5
+    nameserver 223.6.6.6
+    options edns0 trust-ad
+  '';
+
+  systemd.network.networks."10-eth0" = {
+    DHCP = "yes";
+    matchConfig.Name = "eth0";
+  };
+}
