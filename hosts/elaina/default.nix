f28cd128053d058efb0cbce06b89dc82684678a4
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-02-01 17:05:45 +0800

    feat(elaina): add game tools

--- hosts/elaina/default.nix
+++ hosts/elaina/default.nix
@@ -15,14 +15,51 @@
 
   networking.networkmanager.enable = true;
 
-  hm'.home.packages = with pkgs; [
-    lutris
-    protonplus
+  hm' = {
+    home.packages = with pkgs; [
+      protonplus
 
-    firefox
-    zed-editor
-    neovim
-  ];
+      firefox
+      zed-editor
+      neovim
+      nvtopPackages.amd
+    ];
+
+    # a GUI game launcher for Steam/GoG/Epic
+    # https://lutris.net/games?ordering=-popularity
+    programs.lutris = {
+      enable = true;
+      defaultWinePackage = pkgs.proton-ge-bin;
+      steamPackage = pkgs.steam;
+      winePackages = with pkgs; [
+        wineWow64Packages.full
+        wineWowPackages.stagingFull
+      ];
+      extraPackages = with pkgs; [
+        winetricks
+        gamescope
+        gamemode
+        mangohud
+        umu-launcher
+      ];
+    };
+  };
+
+  preservation' = {
+    user.directories = [
+      ".local/share/umu"
+      ".local/share/lutris"
+      ".local/share/zed"
+      ".cache/ProtonPlus"
+      ".cache/mozila"
+      ".config/zed"
+    ];
+  };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
   services'.vnstat.enable = true;
   services'.openssh.enable = true;
