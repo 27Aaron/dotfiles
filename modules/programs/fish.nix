f3a7fc9a2ce3131f80d4f1ff2b892815e027f8c9
Aaron <85681241+27Aaron@users.noreply.github.com>
2026-04-03 11:04:44 +0800

    feat: restructure fish configuration for improved clarity and organization

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -1,3 +1,16 @@
 {
-  programs.fish.enable = true;
+  hm'.programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      # Disable the greeting message.
+      set fish_greeting
+
+      # Load Starship.
+      starship init fish | source
+
+      # Add uv and uvx shell completions
+      uv generate-shell-completion fish | source
+      uvx --generate-shell-completion fish | source
+    '';
+  };
 }
