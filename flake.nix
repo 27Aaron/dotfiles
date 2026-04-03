884b3fe10d1872e0f4a4c0dbd8e4a2de394cd6dc
Aaron <niceboy@duck.com>
2026-04-03 09:41:41 +0800

    feat: 初始化 macOS 声明式配置
    添加 nix-darwin、Home Manager、Homebrew、主机发现、常用程序与系统默认设置。

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Aaron's macOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    forEachSystem = lib.genAttrs lib.systems.flakeExposed;
+  in {
+    darwinModules = import ./modules {inherit lib inputs;};
+    darwinConfigurations = import ./hosts {inherit self inputs lib;};
+
+    # nix code formatter
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+  };
+}
