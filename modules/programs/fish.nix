6d4831add6403c886b61570ead1e6e184d25d717
Aaron <niceboy@duck.com>
2026-04-03 10:26:37 +0800

    feat(shell): 将交互环境迁移至 Fish
    启用 Homebrew、Atuin、Eza 与 Starship 的 Fish 集成，并将 Fish 注册为系统 Shell。

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -1,15 +1,7 @@
 {
-  hm'.programs = {
+  programs = {
     fish = {
       enable = true;
-      interactiveShellInit = ''
-        # homebrew
-        if test -x /opt/homebrew/bin/brew
-          eval (/opt/homebrew/bin/brew shellenv)
-        else if test -x /usr/local/bin/brew
-          eval (/usr/local/bin/brew shellenv)
-        end
-      '';
     };
   };
 }
