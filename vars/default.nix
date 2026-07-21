d678a563be22b4c587045a51d3ef4e512223c949
Aaron <niceboy@duck.com>
2026-07-21 16:48:57 +0800

    chore(vars): 更新用户密码哈希

--- vars/default.nix
+++ vars/default.nix
@@ -7,7 +7,7 @@
 
   stateVersion = "26.11";
 
-  hashedPassword = "$y$j9T$9BVbJKhiRZ/U5iTL7sZtT/$3xUVDretSE/RqiacfJbu/vK0Li0H8Z/S4LESEj1E/u1";
+  hashedPassword = "$y$j9T$fXIHIyb1usprTzAw.ntqJ/$I/sbwudS.KESDGLwKV8QzsLqr7pNQvYYv20GcWKFsV1";
 
   sshAuthorizedKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6xNNhF6jaPKuch8vSHwHTGlbyn4i2zSHxrqGOiacxG Aaron@Deployment"
