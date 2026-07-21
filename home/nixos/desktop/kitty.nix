2e07faa9be7509d2e60fc88e4c3bee722872755e
Aaron <niceboy@duck.com>
2026-07-21 20:50:42 +0800

    feat(nixos): 配置 Kitty 终端

---
+++ home/nixos/desktop/kitty.nix
@@ -0,0 +1,23 @@
+{
+  programs.kitty = {
+    enable = true;
+
+    font = {
+      name = "Maple Mono NF CN";
+      size = 12;
+    };
+
+    settings = {
+      background_opacity = "0.8";
+      confirm_os_window_close = 0;
+      hide_window_decorations = "titlebar-only";
+      mouse_hide_wait = 2;
+      tab_bar_edge = "top";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      url_color = "#ffb4a8";
+      url_style = "dotted";
+      window_padding_width = 10;
+    };
+  };
+}
